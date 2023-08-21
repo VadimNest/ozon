873595042d5340aaaef2ff789af62c48d566a0ee
Vadim <mr.kostura@mail.ru>
2023-08-21 12:54:23 +0300

    Подключил иконки

--- components/map-block/map-block.vue
+++ components/map-block/map-block.vue
@@ -7,14 +7,25 @@
         :zoom="mapConfig.zoom"
         :coordinates="mapConfig.coordinates"
       >
-        <!--Markers-->
+        <YandexClusterer :options="{ preset: 'islands#nightClusterIcons' }">
+          <YandexMarker
+            v-for="point in points"
+            :key="`1-marker-${point.id}`"
+            :coordinates="getCoords(point.coordinates)"
+            :marker-id="`1-marker-${point.id}`"
+            :options="{
+              iconLayout: 'default#image',
+              iconImageHref: '/icons/marker.svg',
+            }"
+          />
+        </YandexClusterer>
       </YandexMap>
     </ClientOnly>
   </div>
 </template>
 
 <script setup>
-import { YandexMap, YandexMarker } from 'vue-yandex-maps';
+import { YandexMap, YandexClusterer, YandexMarker } from 'vue-yandex-maps';
 
 onMounted(async () => {
   points.value = await $fetch('http://localhost:3000/points');
@@ -27,12 +38,17 @@ const mapConfig = {
   zoom: 12,
   coordinates: [45.043317, 41.96911],
 };
+
 const settings = {
   apiKey: runtimeConfig.yandexAPI,
   lang: 'ru_RU',
   coordorder: 'latlong', // Порядок задания географических координат
   debug: false, // Режим отладки
   version: '2.1',
+};
+
+const getCoords = (coords) => {
+  return [coords.latitude, coords.longitude];
 };
 </script>
 
