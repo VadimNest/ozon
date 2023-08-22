939593d9f778c01c5f97c990ad34ed5bb042b2c9
Vadim <mr.kostura@mail.ru>
2023-08-22 09:57:11 +0300

    Внедрил динамику в список точек

--- components/aside-block/aside-block.vue
+++ components/aside-block/aside-block.vue
@@ -1,7 +1,12 @@
 <template>
   <aside class="aside-block">
     <ul class="aside-block__list">
-      <li class="aside-block__item" v-for="point in points" :key="point.id">
+      <li
+        class="aside-block__item"
+        v-for="point in points"
+        :key="point.id"
+        @click="сlickMarker(point)"
+      >
         <h2 class="aside-block__title">Пункт Ozon</h2>
         <h3 class="aside-block__subtitle">
           {{ point.address }}
